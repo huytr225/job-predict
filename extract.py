879481d1db72bf35e070f2549d911fe652f05c5b
Huy <huytr225@gmail.com>
2017-07-15 04:31:15 +0000

    update

---
+++ extract.py
@@ -0,0 +1,50 @@
+
+from pyspark.sql.session import SparkSession as spark
+import pandas as pd
+from pyspark import SparkContext
+from pyspark.sql import SQLContext
+from pyspark.sql.types import *
+from matplotlib import pyplot
+import os
+sc = SparkContext(appName="Task_usage")
+sql_context = SQLContext(sc)
+
+# folder_path ='/mnt/volume/ggcluster/clusterdata-2011-2/task_usage/'
+folder_path = '/mnt/volume/ggcluster/clusterdata-2011-2/task_usage/'
+
+dataSchema = StructType([StructField('startTime', StringType(), True),
+                         StructField('endTime', StringType(), True),
+                         StructField('JobId', LongType(), True),
+                         StructField('taskIndex', LongType(), True),
+                         StructField('machineId', LongType(), True),
+                         StructField('meanCPUUsage', FloatType(), True),
+                         # canonical memory usage
+                         StructField('CMU', FloatType(), True),
+                         # assigned memory usage
+                         StructField('AssignMem', FloatType(), True),
+                         # unmapped page cache memory usage
+                         StructField('unmapped_cache_usage', FloatType(), True),
+                         StructField('page_cache_usage', FloatType(), True),
+                         StructField('max_mem_usage', FloatType(), True),
+                         StructField('mean_diskIO_time', FloatType(), True),
+                         StructField('mean_local_disk_space', FloatType(), True),
+                         StructField('max_cpu_usage', FloatType(), True),
+                         StructField('max_disk_io_time', FloatType(), True),
+                         StructField('cpi', FloatType(), True),
+                         StructField('mai', FloatType(), True),
+                         StructField('sampling_portion', FloatType(), True),
+                         StructField('agg_type', FloatType(), True),
+                         StructField('sampled_cpu_usage', FloatType(), True)])
+
+jobEventsDF = (
+    sql_context.read
+    .format('com.databricks.spark.csv')
+    .schema(dataSchema)
+    .load("../../clusterdata-2011-2/task_usage/part-00000-of-00500.csv")
+)
+jobEventsDF.createOrReplaceTempView("dataFrame")
+df = sql_context.sql("select count(distinct JobID) from dataFrame")
+schema_df = ["time", "numberOfJob"]
+df.toPandas().to_csv('results/extract.csv', index=False, header=None)
+sc.stop()
+
